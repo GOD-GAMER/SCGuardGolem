1805725aa46157bfcb848551532b5e790bb988b9
GOD-GAMER <cabcab4512@gmail.com>
2026-04-08 03:05:55 -0400

    Fix EventBusSubscriber annotation for NeoForge 1.21.10

--- src/main/java/net/geforcemods/scguardgolem/client/SCGClientEvents.java
+++ src/main/java/net/geforcemods/scguardgolem/client/SCGClientEvents.java
@@ -7,7 +7,7 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 
-@EventBusSubscriber(modid = SCGuardGolem.MODID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(modid = SCGuardGolem.MODID, value = Dist.CLIENT)
 public class SCGClientEvents {
 
     @SubscribeEvent
