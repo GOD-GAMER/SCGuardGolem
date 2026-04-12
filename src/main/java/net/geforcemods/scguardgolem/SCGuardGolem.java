96c191190f6926657990382204ce25f7db9a789e
GOD-GAMER <cabcab4512@gmail.com>
2026-04-11 23:17:45 -0400

    Bump to 1.1.0 with VERSION constant

--- src/main/java/net/geforcemods/scguardgolem/SCGuardGolem.java
+++ src/main/java/net/geforcemods/scguardgolem/SCGuardGolem.java
@@ -24,6 +24,7 @@ import com.mojang.logging.LogUtils;
 @Mod(SCGuardGolem.MODID)
 public class SCGuardGolem {
     public static final String MODID = "scguardgolem";
+    public static final String VERSION = "1.1.0";
     public static final Logger LOGGER = LogUtils.getLogger();
     public static boolean scLoaded;
 
