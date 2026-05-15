847487f66a1ffaa3164aa1eaad72aeec83f3f74a
GOD-GAMER <cabcab4512@gmail.com>
2026-05-14 23:54:36 -0400

    fix: compile with Java 17 toolchain for Forge 1.20.1 compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ base {
     version = modVersion
 }
 
-java.toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 
 minecraft {
     mappings("official", mcVersion)
